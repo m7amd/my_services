40fac5660f231ad7d8f2961eed7ea94cdcfc09a6
m7amd <mohammadkhaseeb6@gmail.com>
2024-02-16 21:13:36 +0200

    updates

---
+++ khdmati.sql
@@ -0,0 +1,16 @@
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`aemail`, `apassword`) VALUES
+('admin@mail.com', '123');
+
